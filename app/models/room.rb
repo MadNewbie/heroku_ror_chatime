c48105a9a199aea1fd683702fbb2b6bcbb556601
Rachmad Yanuarianto <rachmad.y25@gmail.com>
2024-06-05 06:06:53 +0700

    fix small things

--- app/models/room.rb
+++ app/models/room.rb
@@ -13,7 +13,7 @@ class Room < ApplicationRecord
     def self.create_private_room(users, room_name)
         single_room = Room.create(name: room_name, is_private: true)
         users.each do |user|
-            Participan.create(user_id: user.id, room_id: single_room.id)
+            Participant.create(user_id: user.id, room_id: single_room.id)
         end
         single_room
     end
